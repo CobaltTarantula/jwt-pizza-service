b47741fd08042ce47ef3eafa34fc9718acaa1f34
Zack <zack.r.clifford@gmail.com>
2026-01-27 15:36:52 -0700

    outline for franchiseRouter tests

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,14 @@
+// const request = require('supertest');
+// const app = require('../service');
+
+// test get franchises route
+
+// test get user franchises route
+
+// test create franchise route
+
+// test delete franchise route
+
+// test create store route
+
+// test delete store route
